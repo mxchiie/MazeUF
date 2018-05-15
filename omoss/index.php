1cf198a97e60d4c4bcc6aa421a83486cdec4763b
tevandar <tom.kulsiri.ee15a@gmail.com>
2018-05-15 09:25:32 +0200

    uppdaterat omos

--- omoss/index.php
+++ omoss/index.php
@@ -17,12 +17,16 @@
 		?>
 		
 		<h1> Om oss </h1>
-		<p> MaZe UF består av 5 ungdomar från Chalmers som arrangerar LAN som ett sätt att sprida vetskapen om E-Sport. <br>
+		<p> MaZe UF består av 5 ungdomar från NTI Gymnasiet som arrangerar LAN som ett sätt att sprida vetskapen om E-Sport. <br>
 		Vi vill att alla oavsett ålder ska kunna vara med och delta i våra LAN, inte bara för att det är roligt, <br>
 		utan även för att det kan vara en bra möjlighet att lära känna andra människor med samma intressen. <br>
 		<br>
 		Vi på MaZe UF samarbetar med Pink Orange som också är en arrangör i Göteborg som försöker sprida ordet om E-Sporten - <br>
 		Bland annat genom workshops, föreläsningar och LAN.
+		<br>
+		<br>
+		Vi samarbetar även med skolans Elevkår där vi gemensamt planerar och arrangerar lan i skolan för skolans elever.
+	
 		
 		</p>
 		
@@ -33,8 +37,12 @@ Följ oss på sociala medier
 <nav>
 <p>
 Vi finns på Facebook och Instagram<br>
+<a href="https://www.facebook.com/UFMaZe/">
 <img src="../bilder/fejsbuk.png" width="50"> 
-
+</a>
+<a href="https://www.instagram.com/ufmaze/">
+<img src="../bilder/instegrum.png" width="50"> 
+</a>
 <br>
 Följ oss gärna för mer information <br>
 <br>
